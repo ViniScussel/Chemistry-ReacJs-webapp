1b60fe344aadedd5e549fbdfd1ce1a62e7fdf4d2
Scussel <85595449+ViniScussel@users.noreply.github.com>
2023-03-18 23:03:05 -0300

    Updated some functions on app:
    -added a "stop spinnin atom" when onMouseEnter
    -updated content (nomenclatures and associated)
    -added a border-bottom on menu
    -changed background and text color

--- ReactChemistry/my-app/src/App.js
+++ ReactChemistry/my-app/src/App.js
@@ -1,12 +1,21 @@
+//cd my-app
+//npm start
 import logo from './logo.svg';
 import './App.css';
+
+function pare(){
+  document.getElementById("rodarodajequiti").className='naoroda';
+}
+function contnue(){
+  document.getElementById("rodarodajequiti").className='ReactImg';
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-menu">
         <div className='App-menu-div'>
-          <img src={logo} className="ReactImg" alt='logo'/>
+          <img src={logo} id='rodarodajequiti' className="ReactImg" alt='logo' onMouseEnter={pare} onMouseLeave={contnue}/>
           <p className='Pgeneral'>
             A responsive ReactJS web app to chemistry classes
           </p>
@@ -31,16 +40,23 @@ function App() {
           </p>
 
           <ul className='ULgeneral'>
-            <li><strong>By ionizable 'H's:</strong> (1H) monobasic acid, (2H) dibasic acid, (3H) tribasic acid;</li>
+            <li><strong>By ionizable 'H's:</strong> (1H) monoacid acid, (2H) diacid acid, (3H) triacid acid;</li>
             <li><strong>By oxigen presence:</strong> oxy-acid when have 'O', and hydracid when not;</li>
-            <li><strong>By their force:</strong> "n°O - n°H = R" (R=0 weak; R=1 'semistrong'; R&gt;=2 strong). </li>
+            <li><strong>By their force:</strong> "n°O - n°H = R" (R=0 weak; R=1 'semi'; R&gt;=2 strong).</li>
           </ul>
-
-          
+          <center>
+            <h2>Nomenclatures</h2>
+            <p className='Pwrite'>Also, acids have their own nomenclatures. they are maked by analising  the anions nomenclatures. So, if the anion nomenclature terminate with --, you'll be replacing by -- (for portuguese only):</p>
+            
+          <ul className='ULgeneral'>
+            <li>ATO ‎ ‎ ‎ =&gt; ‎ ‎ ICO</li>
+            <li>ETO =&gt; IDRICO</li>
+            <li>ITO ‎ ‎ ‎  =&gt; ‎ ‎ OSO</li>
+          </ul>  
+          </center>
         </div>
       </header>
     </div>
   );
 }
-
 export default App;
